7749d38f7acd15ed073fe1511553fe9dbb36b7f0
Robson Alves DNX <robson.alves@192-168-1-205.tpgi.com.au>
2024-01-29 11:12:05 +1000

    setting to publish

--- routes/login.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET login page. */
-router.get('/login', function(req, res, next) {
-  res.render('login.ejs', { title: 'Login' });
-});
-
-module.exports = router;
